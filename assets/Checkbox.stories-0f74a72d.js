1260d8ead26adb892cc067073e2628fc07193a5f
Alex <alex@finset.io>
2023-10-23 14:42:12 +0900

    Updates

---
+++ assets/Checkbox.stories-0f74a72d.js
@@ -0,0 +1,35 @@
+import{j as e}from"./jsx-runtime-c301395f.js";import{r as x}from"./_baseForOwn-4ea03334.js";import{u as p,F as f}from"./index-93002463.js";import"./index-ea6158c3.js";import{C as r}from"./index-692f7a1b.js";import{B as b,T as g}from"./index-1a378a33.js";import"./_commonjsHelpers-de833af9.js";import"./index-9d475cdf.js";import"./toNumber-b8059529.js";const E={title:"DESIGN-SYSTEM/forms/Checkbox",component:r,tags:["autodocs"],argTypes:{label:{description:"checkbox의 라벨. 필수가 아닙니다."},name:{description:"checkbox의 name. 고유한 이름입니다."},size:{control:"inline-radio",options:["lg","md","sm","xs"]},color:{control:"inline-radio",options:["primary","tertiary"]},reverse:{description:"checkbox의 icon과 label의 좌우 위치를 바꿉니다."},submit:{description:"form 요소 여부입니다."}}},k=s=>{const[a,m]=x.useState(!1),d=()=>{m(t=>!t)},l=p({defaultValues:{"storybook-checkbox":!1}}),{handleSubmit:u}=l,h=t=>{alert(JSON.stringify(t))};return e.jsx(e.Fragment,{children:s.hasContext?e.jsx(f,{...l,children:e.jsxs("form",{className:"flex flex-col",onSubmit:u(h),children:[e.jsx(r,{...s}),e.jsx(b,{type:"submit",className:"mt-8",children:"결과 확인"})]})}):e.jsxs("div",{className:"flex flex-col",children:[e.jsx(r,{...s,checked:a,onChange:d}),e.jsx("div",{className:"mt-10",children:a&&e.jsx(g,{variant:"head",size:"lg",color:"text-gray-900",children:"checked"})})]})})},o=k.bind({});o.args={label:"label",name:"storybook-checkbox",size:"md",color:"primary",reverse:!1,hasContext:!1,disabled:!1};var c,i,n;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`args => {
+  const [selectedValue, setSelectedValue] = useState(false);
+  const selectionHandler = () => {
+    setSelectedValue(prev => !prev);
+  };
+  const formMethods = useForm<FormFields>({
+    defaultValues: {
+      'storybook-checkbox': false
+    }
+  });
+  const {
+    handleSubmit
+  } = formMethods;
+  const onSubmit = (data: unknown) => {
+    alert(JSON.stringify(data));
+  };
+  return <>
+      {args.hasContext ? <FormProvider {...formMethods}>
+          <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+            <Checkbox {...args} />
+            <Button type="submit" className="mt-8">
+              결과 확인
+            </Button>
+          </form>
+        </FormProvider> : <div className="flex flex-col">
+          <Checkbox {...args} checked={selectedValue} onChange={selectionHandler} />
+          <div className="mt-10">
+            {selectedValue && <Typography variant="head" size="lg" color="text-gray-900">
+                checked
+              </Typography>}
+          </div>
+        </div>}
+    </>;
+}`,...(n=(i=o.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const M=["Default"];export{o as Default,M as __namedExportsOrder,E as default};
+//# sourceMappingURL=Checkbox.stories-0f74a72d.js.map
---
+++ assets/Toggle.stories-45c87028.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-c301395f.js";import{r as T}from"./_baseForOwn-4ea03334.js";import{u as x,F as h}from"./index-93002463.js";import{B as v,T as S}from"./index-1a378a33.js";import"./index-ea6158c3.js";import{T as l}from"./index-692f7a1b.js";import"./_commonjsHelpers-de833af9.js";import"./index-9d475cdf.js";import"./toNumber-b8059529.js";const O={title:"DESIGN-SYSTEM/forms/Toggle",component:l,tags:["autodocs"],argTypes:{}},y=o=>{const[r,a]=T.useState(!1),m=x({defaultValues:{"storybook-toggle":1}}),{handleSubmit:f}=m,b=g=>{alert(JSON.stringify(g))};return e.jsx(e.Fragment,{children:o.hasContext?e.jsx(h,{...m,children:e.jsxs("form",{onSubmit:f(b),className:"flex flex-col",children:[e.jsx(l,{...o}),e.jsx(v,{type:"submit",className:"mt-8",children:"결과 확인"})]})}):e.jsxs("div",{className:"flex flex-col",children:[e.jsx(l,{...o,isToggled:r,onToggle:()=>a(g=>!g)}),e.jsx("div",{className:"mt-8",children:e.jsxs(S,{children:["isToggled: ",r?"true":"false"]})})]})})},s=y.bind({});s.args={name:"storybook-toggle",options:[{value:1,label:"직접입력"},{value:2,label:"마이데이터"}],hasContext:!0};const N=()=>{const[o,r]=T.useState(!1);return e.jsx(e.Fragment,{children:e.jsx(l,{name:"storybook-toggle-2",label:"storybook toggle 2",isToggled:o,onToggle:()=>r(a=>!a),options:[{value:1,label:"직접입력"},{value:2,label:"마이데이터"}]})})},t=N.bind({});t.args={};var n,i,d;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`args => {
+  const [isToggled, setIsToggled] = useState(false);
+  const formMethods = useForm<FormFields>({
+    defaultValues: {
+      'storybook-toggle': 1
+    }
+  });
+  const {
+    handleSubmit
+  } = formMethods;
+  const onSubmit = (data: unknown) => {
+    alert(JSON.stringify(data));
+  };
+  return <>
+      {args.hasContext ? <FormProvider {...formMethods}>
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+            <Toggle {...args} />
+            <Button type="submit" className="mt-8">
+              결과 확인
+            </Button>
+          </form>
+        </FormProvider> : <div className="flex flex-col">
+          <Toggle {...args} isToggled={isToggled} onToggle={() => setIsToggled(prev => !prev)} />
+
+          <div className="mt-8">
+            <Typography>isToggled: {isToggled ? 'true' : 'false'}</Typography>
+          </div>
+        </div>}
+    </>;
+}`,...(d=(i=s.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var c,u,p;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const [isToggled, setIsToggled] = useState(false);
+  return <>
+      <Toggle name="storybook-toggle-2" label="storybook toggle 2" isToggled={isToggled} onToggle={() => setIsToggled(prev => !prev)} options={[{
+      value: 1,
+      label: '직접입력'
+    }, {
+      value: 2,
+      label: '마이데이터'
+    }]} />
+    </>;
+}`,...(p=(u=t.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};const P=["Default","NotUsingForm"];export{s as Default,t as NotUsingForm,P as __namedExportsOrder,O as default};
+//# sourceMappingURL=Toggle.stories-45c87028.js.map
